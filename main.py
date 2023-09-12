54516f6a0d2476511398f7377b627829a56f8bf0
David Singh <david@metaphysic.ai>
2023-09-12 11:35:12 +0100

    added setup.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from src.hello_world import hello_world
+from hello_world.hello_world import hello_world
 
 if __name__ == "__main__":
     print(hello_world())
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='hello-world',
+    version='1.0.0',
+    packages=['hello_world'],
+    url='',
+    license='',
+    author='David Singh',
+    author_email='',
+    description='Test project'
+)
